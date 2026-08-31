c255a432bbea256308510cfaa1e5a842075efb17
Asfak <skasfakahamed495@gmail.com>
2026-08-31 10:45:17 +0530

    Project_Interceptors

--- day-16-Projects_Interceptors/src/Components/Navbar.jsx
+++ day-16-Projects_Interceptors/src/Components/Navbar.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
-import { NavLink } from 'react-router'
+import React from "react";
+import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
-    <div className='border-r border-gray-400'>
-      <h1>E-comm</h1>
-      <div className='flex flex-col gap-10'>
-        <NavLink to={"/main"}>Home</NavLink>
-        <NavLink to={"/main/users"}>Users</NavLink>
-        <NavLink to={"/main/product"}>Product</NavLink>
+    <div className="border-r border-gray-400 flex flex-col gap-7">
+      <h1 className="text-2xl font-semibold">E-comm</h1>
+      <div className="flex flex-col gap-6 ml-5">
+        <NavLink
+          className={({ isActive }) => {
+            isActive
+              ? "font-semibold text-red-500 border-b border-gray-500"
+              : "text-black border-b border-gray-500";
+          }}
+          to={"/main"}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => {
+            isActive
+              ? "font-semibold text-red-500 border-b border-gray-500"
+              : "text-black border-b border-gray-500";
+          }}
+          to={"/main/users"}
+        >
+          Users
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => {
+            isActive
+              ? "font-semibold text-red-500 border-b border-gray-500"
+              : "text-black border-b border-gray-500";
+          }}
+          to={"/main/product"}
+        >
+          Product
+        </NavLink>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
