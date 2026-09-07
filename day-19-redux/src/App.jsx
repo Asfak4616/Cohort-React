cf63fa406dc36ab1c7b17ea775987e7df4ba13aa
Asfak <skasfakahamed495@gmail.com>
2026-09-07 11:10:22 +0530

    Redux_toolkit

--- day-19-redux/src/App.jsx
+++ day-19-redux/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { decrement, increment } from './Features/CounterSlice';
+import {useDispatch,useSelector} from "react-redux"
 
 const App = () => {
+  let dispatch = useDispatch()
+ let {count} = useSelector((Store)=>Store.counter);
   return (
     <div>
-      <h1>Reduxjs-toolkit</h1>
+   <h1>count is {count}</h1>
+   <button onClick={()=>dispatch(decrement())}>Decrement</button>
+   <button onClick={()=>dispatch(increment())}>Increment</button>
     </div>
   )
 }
